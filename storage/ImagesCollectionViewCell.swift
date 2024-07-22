24f2293d7bcc52cad0a8fc40728548eeb2d924c6
MaratXcode <kosjachok8@yandex.ru>
2024-07-23 00:51:55 +0300

    cach worked

---
+++ storage/ImagesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesCollectionViewCell.swift
+//  storage
+//
+//  Created by Marat Fakhrizhanov on 22.07.2024.
+//
+
+import UIKit
+
+class ImagesCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ storage/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  storage
+//
+//  Created by Marat Fakhrizhanov on 22.07.2024.
+//
+
+import Foundation
